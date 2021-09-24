92b0b78567db0375fa32f8bcbcb0ed0b1da51b68
LizethGaray21 <89488759+LizethGaray21@users.noreply.github.com>
2021-09-24 10:36:11 -0700

    creacion de folder

---
+++ Programa1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+//#include<conio.h>
+//#include<math.h>
+using namespace std;
+
+//Instituo Tecnologico de Tijuana
+//Alumna: Garay Reyes Lizeth #19210491
+//C3:P16: Dados los tres lados de cualquier triangulo
+//ABC, calcular e imprimir el area de ese triangulo.
+
+int main() {
+
+    float b=8,h=25, area;
+    cout <<"Ingrese la base:" <<b<<endl;
+    cin>>b;
+    cout<<"Ingrese la altura: "<<h<<endl;
+    cin>>h;
+    area=(b * h)/2;
+    cout <<"El area del triangulo es: " <<area<<endl;
+
+}
